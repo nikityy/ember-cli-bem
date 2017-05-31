6268ac97506e8e146edff87629e474a1c248f8b9
Nikita Gusarov <nickgusarov@gmail.com>
2017-05-31 10:34:30 +0300

    Added htmlbars plugin.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* eslint-env node */
 'use strict';
 
+const HtmlbarsPlugin = require('./lib/htmlbars-plugin');
+
 module.exports = {
-  name: 'ember-cli-bem'
+
+  name: 'ember-cli-bem',
+
+  setupPreprocessorRegistry: function(type, registry) {
+    // Skip if we're setting up this addon's own registry
+    if (type !== 'parent') { return; }
+
+    registry.add('htmlbars-ast-plugin', {
+      name: 'ember-css-modules',
+      plugin: HtmlbarsPlugin,
+      baseDir: function() {
+        return __dirname;
+      }
+    });
+  },
+
 };
