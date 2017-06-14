70de140676512e2e29c704e0228bd80adf77da35
Nikita Gusarov <nickgusarov@gmail.com>
2017-06-14 15:03:57 +0300

    Added mods support in elem helper.

--- addon/helpers/elem.js
+++ addon/helpers/elem.js
@@ -1,17 +1,25 @@
 import Ember from 'ember';
-import { elem } from 'ember-cli-bem/mixins/bem';
+import { elem, mod } from 'ember-cli-bem/mixins/bem';
 
 const {
   HTMLBars: { makeBoundHelper },
 } = Ember;
 
 export default makeBoundHelper(function(params, hash) {
-  const { blockName } = hash;
+  const { blockName } = hash
   const [ elemName ] = params;
 
   if (!blockName) {
     throw Error(`blockName is required for 'elem' helper`);
   }
 
-  return elem(blockName, elemName);
+  const elemClassName = elem(blockName, elemName);
+
+  const modNames = Object.keys(hash).filter((key) => key !== 'blockName');
+  const modClassNames = modNames.map((modName) => {
+    const modValue = hash[modName];
+    return mod(elemClassName, { modName, modValue });
+  });
+
+  return [elemClassName, ...modClassNames].filter(Boolean).join(' ');
 });
